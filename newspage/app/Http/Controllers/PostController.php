54db30d50b8e23b175468f51da141122d61ae14b
Admir Sahman <a15admsa@student.his.se>
2018-03-15 15:23:42 +0100

    New updates to Laravel

--- newspage/app/Http/Controllers/PostController.php
+++ newspage/app/Http/Controllers/PostController.php
@@ -6,16 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class PostController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index($id)
-    {
-      return "It's working: " . $id;
+class PostController extends Controller{
+    public function index($id){
+      return view('post', compact('id'));
     }
 }
 
