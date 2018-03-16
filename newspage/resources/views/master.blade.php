f60bb21cb4c2da4f53c190ad88b7283f42cab551
Admir Sahman <a15admsa@student.his.se>
2018-03-16 15:05:13 +0100

    Added style and functionality to the Laravel project

--- newspage/resources/views/master.blade.php
+++ newspage/resources/views/master.blade.php
@@ -3,8 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>Laravel</title>
+    <link href="{{ asset('style.css') }}" rel="stylesheet">
   </head>
   <body>
+    <a href="/newspage/public">
+      <img src="{{asset('logo.png')}}" alt="logo" id="logo">
+    </a>
+    <div class="form">
+      <input type="text" id="searchBox" name="searchBox" size="40" maxlength="128">
+      <input type="submit" name="search" value="Search">
+    </div>
     <h1>@yield('content')</h1>
+
   </body>
 </html>
